a6128b7bfa035ccf858a390bd2f86a8fd0629bde
MayNor <wen.net@yandex.ru>
2023-10-03 12:51:18 +0300

    Rework: route

---
+++ src/Lab1/Entity/Route/Path.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Itmo.ObjectOrientedProgramming.Lab1.Interfaces;
+using Itmo.ObjectOrientedProgramming.Lab1.Models.Results;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Entity.Route;
+
+public class Path
+{
+    private IEnviroment[] _enviroments;
+
+    public Path(IEnumerable<IEnviroment> enviroments)
+    {
+        _enviroments = enviroments.ToArray();
+    }
+
+    private PassingPathResult LetShip(IShip ship)
+    {
+        foreach (IEnviroment enviroment in _enviroments)
+        {
+            PassageResult result = enviroment.CalculationPassage(ship);
+
+            if (result is PassageResult.Success)
+                continue;
+
+            if (result is PassageResult.ImpossibleOvercome)
+                return new PassingPathResult.Impossible();
+
+            if (result is PassageResult.CrewDied)
+                return new PassingPathResult.CrewDeath();
+
+            if (result is PassageResult.ShipDestroyed)
+                return new PassingPathResult.DestructionShip();
+        }
+
+        return new PassingPathResult.Success();
+    }
+}
---
+++ src/Lab1/Models/Results/PassingPathResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Models.Results;
+
+public record PassingPathResult
+{
+    private PassingPathResult() { }
+
+    public record Success : PassingPathResult;
+
+    public record ShipLoss : PassingPathResult;
+
+    public record DestructionShip : PassingPathResult;
+
+    public record CrewDeath : PassingPathResult;
+
+    public record Impossible : PassingPathResult;
+}
